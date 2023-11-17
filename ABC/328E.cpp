d7e2b1f5dd74deb1124c3134f91d6e1957cdb288
sakurmoti <105205684+sakurmoti@users.noreply.github.com>
2023-11-17 15:47:52 +0900

    20231117

--- ABC/328E.cpp
+++ ABC/328E.cpp
@@ -12,15 +12,50 @@ vector<vector<ll>> G;
 
 int main(){
     cin >> N >> M >> K;
+
     G.resize(N+1, vector<ll>(N+1, 1e18));
-    vector<pair<int, int>> sides(M);
+    vector<pair<int, int>> edges(M);
     for(int i = 0; i < M; i++){
         ll u,v,w; cin >> u >> v >> w;
-        sides[i].push_back(make_pair(u,v));
+        u--;
+        v--;
+        edges[i] = (make_pair(u,v));
         G[u][v] = w;
         G[v][u] = w;
     }
 
-    vector<int> comb(M);
+    // M個の辺からN-1個の辺を選ぶ
+    vector<bool> comb(M, 0);
+    for(int i = 0; i < N-1; i++){
+        comb[i] = 1;
+    }
+    sort(ALL(comb));
+
+    ll ans = K;
+    do
+    {
+        dsu d(N);
+        ll cost = 0;
+        bool SpanningTree = true;
+
+        for(int i = 0; i < M; i++){
+            if(comb[i]){
+                int a = edges[i].first;
+                int b = edges[i].second;
+
+                if(d.same(a,b)) SpanningTree = false;
+                d.merge(a, b);
+                cost += (G[a][b]%K);
+            }
+        }
+
+        if(SpanningTree){
+            ans = min(ans, cost%K);
+        }
+
+
+    } while (next_permutation(ALL(comb)));
+    
+    cout << ans << endl;
     return 0;
 }
